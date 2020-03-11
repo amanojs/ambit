995b98e0ff5ad4e8293c1f16d82160728dc8c3da
tanabe <1422hal@gmail.com>
2020-03-12 07:25:20 +0900

    React + TypeScript + webpack + Babel

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export interface AppProps {
+  compiler: string
+  framework: string
+}
+
+export class App extends React.Component<AppProps, {}> {
+  render() {
+    return (
+      <h1>
+        Hello {this.props.compiler} and {this.props.framework}!
+      </h1>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports = {
+  watch: true,
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'source-map-loader',
+            options: {
+              enforce: 'pre',
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve('./src')],
+    extensions: ['.tsx', '.js', '.ts', 'json']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+}
