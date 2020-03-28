fd84ca223305738ba2758c54cb376a9ae9041f83
amanojs <takashivue07@gmail.com>
2020-03-28 19:48:47 +0900

    Header.tsx作成

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface Props {}
+
+export const Header: React.FC<Props> = props => {
+  return (
+    <React.Fragment>
+      <div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
