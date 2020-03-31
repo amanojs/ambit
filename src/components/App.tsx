36f7634c96690d759cab57b91af9f90f0d0a6963
amanojs <takashivue07@gmail.com>
2020-03-31 13:34:15 +0900

    Nav型完成

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import { Top } from './templates/Top'
+import { Header } from './organisms/Header'
 
 export class App extends React.Component<{}, {}> {
   render() {
-    return <Top compiler="TypeScript" framework="React" />
+    return (
+      <React.Fragment>
+        <Header />
+        <Top compiler="TypeScript" framework="React" />
+      </React.Fragment>
+    )
   }
 }
