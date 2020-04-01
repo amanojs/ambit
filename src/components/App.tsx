c0b45015c5df908cf4efb7b4ec64b7d319be8d9e
amanojs <takashivue07@gmail.com>
2020-04-01 15:53:10 +0900

    悪戦苦闘したけどできた

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react'
 import { Top } from './templates/Top'
+import { Test } from './templates/Test'
 import { Header } from './organisms/Header'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
 
 export class App extends React.Component<{}, {}> {
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Top compiler="TypeScript" framework="React" />
+        <Router>
+          <Header />
+          {/* BrowserRouterだと遷移先でリロードすると404になるのでHashRouterを使用 */}
+          <Switch>
+            <Route exact path="/" component={Top} />
+            <Route exact path="/test" component={Test} />
+            <Route exact path="/posts/:id" component={Test} />
+          </Switch>
+        </Router>
       </React.Fragment>
     )
   }
